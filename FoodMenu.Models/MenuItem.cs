7dcc7675b94cf81d72ed20ece5c5d5e86f3da4e4
Marianne K. A Silva <marikell_silva@hotmail.com>
2017-12-01 20:08:32 -0200

    2008-merge

--- FoodMenu.Models/MenuItem.cs
+++ FoodMenu.Models/MenuItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,15 @@ namespace FoodMenu.Models
     public class MenuItem
     {
         public int IdMenuItem { get; set; }
+        [DisplayName("Nome do Item")]
         public string NamMenuItem { get; set; }
+        [DisplayName("Ordem")]
         public int NumSequence { get; set; }
+        [DisplayName("Preço")]
         public double Price { get; set; }
+        [DisplayName("Descrição")]
         public string DesMenuItem { get; set; }
+        [DisplayName("Cabeçalho")]
         public MenuHeader MenuHeader { get; set; }
         public int IdMenuHeader { get; set; }
 
